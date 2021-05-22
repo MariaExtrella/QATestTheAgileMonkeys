de119a79689af5c8c78f69daf55be190fe265d3e
MariaExtrella <83512148+MariaExtrella@users.noreply.github.com>
2021-05-22 02:04:47 +0200

    Update TestRunner.java

--- apigraphql/src/test/java/test/graphql/TestRunner.java
+++ apigraphql/src/test/java/test/graphql/TestRunner.java
@@ -10,11 +10,3 @@ class TestRunner {
     	// Test that the API met the acceptance criteria for an user different from the admin
         return Karate.run("Test").relativeTo(getClass());
     }  
-    
-/*    @Karate.Test
-    Karate testUsers2() {
-    	System.setProperty("karate.env", "prod");   // set the environment to test
-    	// Test that the API met the acceptance criteria for an user different from the admin
-        return Karate.run("TestUsers").relativeTo(getClass());
-    }      */
-}
